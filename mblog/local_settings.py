95a659c91fd6775343df2b4e195091059be192e4
zhizhuyun <zizhuyun97@gmail.com>
2020-05-28 15:12:49 +0800

    no message

--- mblog/local_settings.py
+++ mblog/local_settings.py
@@ -1,10 +1,9 @@
-import django_heroku
 
 
 # This file is exec'd from settings.py, so it has access to and can
 # modify all the variables in settings.py.
 
-DEBUG = True
+DEBUG = False
 
 # Make these unique, and don't share it with anybody.
 SECRET_KEY = "8)_fhs^$tf-y083*i#zre2e3$z(zztus+o=jyu3=@)#2@w2qy*"
@@ -13,25 +12,8 @@ NEVERCACHE_KEY = "_*h$b)yce43tguq0ua^4&3-spylxw1h4b1%^ax*vd!z69rr9#m"
 DATABASES = {
     "default": {
         # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        # DB name or path to database file if using sqlite3.
-        #"NAME": "dev.db",
-        # Not used with sqlite3. 不适用sqlite3
-        "USER": "",
-        # Not used with sqlite3.
-        "PASSWORD": "",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
-    }
-}
-DATABASES = {
-    "default": {
-        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": 'testdb',
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
         # DB name or path to database file if using sqlite3.
         #"NAME": "dev.db",
         # Not used with sqlite3. 不适用sqlite3
@@ -72,7 +54,5 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "::1","simon-mblog.herokuapp.com"]
 #     "NEVERCACHE_KEY": NEVERCACHE_KEY,
 # }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = "/static/"
-
+import django_heroku
 django_heroku.settings(locals())
