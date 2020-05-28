cddb5bb6f059423d103591b905e9c71b5828ede4
zhizhuyun <zizhuyun97@gmail.com>
2020-05-28 14:56:52 +0800

    no message

--- mblog/local_settings.py
+++ mblog/local_settings.py
@@ -27,10 +27,27 @@ DATABASES = {
         "PORT": "",
     }
 }
-
+DATABASES = {
+    "default": {
+        # Ends with "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'testdb',
+        # DB name or path to database file if using sqlite3.
+        #"NAME": "dev.db",
+        # Not used with sqlite3. 不适用sqlite3
+        "USER": "admin",
+        # Not used with sqlite3.
+        "PASSWORD": "admin",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "5432",
+    }
+}
 # heroku 只支持postgrespool数据库 来自：https://www.jianshu.com/p/610c670eabed
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] = dj_database_url.config()
+
 
 # Allowed development hosts
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "::1","simon-mblog.herokuapp.com"]
