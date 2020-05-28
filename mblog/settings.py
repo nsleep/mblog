bc7d470bcdf74948ab9e2f547024dc85af1e4cca
zhizhuyun <zizhuyun97@gmail.com>
2020-05-28 13:55:58 +0800

    no message

--- mblog/settings.py
+++ mblog/settings.py
@@ -179,7 +179,7 @@ STATIC_URL = "/static/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATIC_ROOT = 'staticfiles'
